ce1bca679a7f76e17f74c21a8e1f49d860bff715
trex-pro <mailto.trexpro@gmail.com>
2026-04-29 16:20:37 +0530

    updated entrypoint for api

--- Project-API/cmd/api/server.go
+++ Project-API/cmd/api/server.go
@@ -2,35 +2,61 @@ package main
 
 import (
 	"crypto/tls"
+	"embed"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"project-api/internal/api/middlewares"
 	"project-api/internal/api/router"
-	"project-api/internal/repositories/sqlconnect"
 	"project-api/pkg/utils"
 	"time"
 
 	"github.com/joho/godotenv"
 )
 
+//go:embed .env
+var envFile embed.FS
+
+func loadEnvEmbeddedFile() {
+	content, err := envFile.ReadFile(".env")
+	if err != nil {
+		log.Fatalf("Error Reading .env File: %v", err)
+	}
+
+	// Creating temp file.
+	temp, err := os.CreateTemp("", ".env")
+	if err != nil {
+		log.Fatalf("Error Creating temp File: %v", err)
+	}
+	defer os.Remove(temp.Name())
+
+	// Wrte contents of env variables to temp file.
+	_, err = temp.Write(content)
+	if err != nil {
+		log.Fatalf("Error Writing to temp File: %v", err)
+	}
+	err = temp.Close()
+	if err != nil {
+		log.Fatalf("Error Closing temp File: %v", err)
+	}
+
+	// Loading env variables to temp file.
+	err = godotenv.Load(temp.Name())
+	if err != nil {
+		log.Fatalf("Error Loading .env File: %v", err)
+	}
+}
+
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		return
-	}
-	_, err = sqlconnect.ConnectDB()
-	if err != nil {
-		utils.ErrorHandler(err, "Error Connecting to DB")
-		return
-	}
+	// Only in Development Stage to run source code.
+	loadEnvEmbeddedFile()
 
 	port := os.Getenv("SERVER_PORT")
 
 	// Load TLS Certificate and Key.
-	cert := "cert.pem"
-	key := "key.pem"
+	cert := os.Getenv("CERT_FILE")
+	key := os.Getenv("KEY_FILE")
 
 	// TLS Config.
 	tlsConfig := &tls.Config{
@@ -42,7 +68,7 @@ func main() {
 		CheckBody:                true,
 		CheckBodyforConetentType: "application/x-www-form-urlencoded",
 		CheckQuery:               true,
-		WhiteList:                []string{"sortBy", "sortOrder", "name", "age", "class"},
+		WhiteList:                []string{"sortBy", "sortOrder", "name", "class"},
 	}
 	const (
 		PathLogin    = "/execs/login"
@@ -72,7 +98,7 @@ func main() {
 	}
 
 	fmt.Println("Server is running on port", port)
-	err = server.ListenAndServeTLS(cert, key)
+	err := server.ListenAndServeTLS(cert, key)
 	if err != nil {
 		log.Fatalln("Error starting server:", err)
 	}
